70875b5af0fd5547d3fce3dc41ead0f64bdf8172
Michał Budziak <michal.mariusz.b@gmail.com>
2016-12-04 23:29:02 +0100

    Add line to AST

--- syntax-checker/AST.py
+++ syntax-checker/AST.py
@@ -1,12 +1,6 @@
 
 class Node(object):
 
-    def accept(self, visitor):
-        return visitor.visit(self)
-
-    def __init__(self):
-        self.children = ()
-
     def __str__(self):
         return self.printTree()
 
@@ -22,8 +16,8 @@ class NodeList(Node):
 
 
 class Const(Node):
-    def __init__(self, value):
-        super(Const, self).__init__()
+    def __init__(self, line, value):
+        self.line = line
         self.value = value
 
 
@@ -44,9 +38,8 @@ class Variable(Node):
 
 
 class BinExpr(Node):
-    def __init__(self, left, op, right):
-        super(BinExpr, self).__init__()
-
+    def __init__(self, line, left, op, right):
+        self.line = line
         self.left = left
         self.op = op
         self.right = right
@@ -62,15 +55,11 @@ class ExpressionList(NodeList):
 
 class GroupedExpression(Node):
     def __init__(self, interior):
-        super(GroupedExpression, self).__init__()
-
         self.interior = interior
 
 
 class FunctionExpression(Node):
     def __init__(self, retType, name, args, body):
-        super(FunctionExpression, self).__init__()
-
         self.retType = retType
         self.name = name
         self.args = args
@@ -79,8 +68,6 @@ class FunctionExpression(Node):
 
 class DeclarationList(Node):
     def __init__(self):
-        super(DeclarationList, self).__init__()
-
         self.declarations = []
 
     def append(self, declaration):
@@ -89,24 +76,20 @@ class DeclarationList(Node):
 
 class Declaration(Node):
     def __init__(self, type, inits):
-        super(Declaration, self).__init__()
-
         self.type = type
         self.inits = inits
 
 
 class InvocationExpression(Node):
-    def __init__(self, name, args):
-        super(InvocationExpression, self).__init__()
-
+    def __init__(self, line, name, args):
+        self.line = line
         self.name = name
         self.args = args
 
 
 class Argument(Node):
-    def __init__(self, type, name):
-        super(Argument, self).__init__()
-
+    def __init__(self, line, type, name):
+        self.line = line
         self.type = type
         self.name = name
 
@@ -121,8 +104,6 @@ class InitList(NodeList):
 
 class Init(Node):
     def __init__(self, name, expr):
-        super(Init, self).__init__()
-
         self.name = name
         self.expr = expr
 
@@ -132,40 +113,32 @@ class InstructionList(NodeList):
 
 
 class PrintInstruction(Node):
-    def __init__(self, expr):
-
-        super(PrintInstruction, self).__init__()
+    def __init__(self, line, expr):
+        self.line = line
         self.expr = expr
 
 
 class LabeledInstruction(Node):
     def __init__(self, id, instr):
-        super(LabeledInstruction, self).__init__()
-
         self.id = id
         self.instr = instr
 
 
 class AssignmentInstruction(Node):
-    def __init__(self, id, expr):
-        super(AssignmentInstruction, self).__init__()
-
+    def __init__(self, line, id, expr):
+        self.line = line
         self.id = id
         self.expr = expr
 
 
 class CompoundInstruction(Node):
     def __init__(self, declarations, instructions):
-        super(CompoundInstruction, self).__init__()
-
         self.declarations = declarations
         self.instructions = instructions
 
 
 class ChoiceInstruction(Node):
     def __init__(self, condition, action, alternateAction=None):
-        super(ChoiceInstruction, self).__init__()
-
         self.condition = condition
         self.action = action
         self.alternateAction = alternateAction
@@ -173,24 +146,19 @@ class ChoiceInstruction(Node):
 
 class RepeatInstruction(Node):
     def __init__(self, instructions, condition):
-        super(RepeatInstruction, self).__init__()
-
         self.instructions = instructions
         self.condition = condition
 
 
 class WhileInstruction(Node):
     def __init__(self, condition, instruction):
-        super(WhileInstruction, self).__init__()
-
         self.condition = condition
         self.instruction = instruction
 
 
 class ReturnInstruction(Node):
-    def __init__(self, expression):
-        super(ReturnInstruction, self).__init__()
-
+    def __init__(self, line, expression):
+        self.line = line
         self.expression = expression
 
 
@@ -204,8 +172,6 @@ class ContinueInstruction(Node):
 
 class Program(Node):
     def __init__(self, program_blocks):
-        super(Program, self).__init__()
-
         self.program_blocks = program_blocks
 
 
@@ -215,8 +181,4 @@ class ProgramBlockList(NodeList):
 
 class ProgramBlock(Node):
     def __init__(self, block):
-        super(ProgramBlock, self).__init__()
-
         self.block = block
-
-
