0cf6bc884a66ea66b366a09afeda26239a5b1516
Michał Budziak <michal.mariusz.b@gmail.com>
2016-12-12 14:38:34 +0100

    Init code interpretation

--- code-interpretation/SymbolTable.py
+++ code-interpretation/SymbolTable.py
@@ -8,15 +8,27 @@ class VariableSymbol(Symbol):
         self.type = type
 
 
+class FunctionSymbol(Symbol):
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+        self.params = []
+
+
 class SymbolTable(object):
-    def __init__(self, parent, name):  # parent scope and symbol table name
+    def __init__(self, parent, name):
         self.parent = parent
         self.name = name
         self.symbols = {}
 
-    def put(self, name, symbol):  # put variable symbol or fundef under <name> entry
+    def put(self, name, symbol):
         self.symbols[name] = symbol
 
-    def get(self, name):  # get variable symbol or fundef from <name> entry
+    def get(self, name):
         return self.symbols.get(name, self.parent.get(name) if self.parent else None)
 
+    def push_scope(self, name):
+        return SymbolTable(self, name)
+
+    def pop_scope(self):
+        return self.parent
